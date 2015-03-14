a5de10856e7ba5234d88288c9719ed3c5796e6a9
vgotsuliak <liddder@gmail.com>
2015-03-14 23:48:22 +0200

    show items on click on row

--- src/main/webapp/spendless.js
+++ src/main/webapp/spendless.js
@@ -3,6 +3,13 @@ var spendlessModule = angular.module('spendless', ["ngResource"]);
 (function () {
     spendlessModule.controller('SpendlessController', function($scope, $resource){
         var spendless = this;
+        $scope.showDetails = function(items) {
+            var text = '';
+            items.forEach(function(item) {
+                text += item.name + ' ' + item.price + ' ' + item.quantity + '\n';
+            });
+            alert(text);
+        };
         spendless.exps = [];
         $scope.expenses = $resource(document.URL + 'expenses');
         var results = $scope.expenses.query(function() {
